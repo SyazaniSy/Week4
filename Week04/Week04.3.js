3d99f0078f8d907cc93fb8d927485bcbf274d93f
SyazaniSy <101816584+SyazaniSy@users.noreply.github.com>
2022-04-03 15:58:04 +0800

    Add files via upload

---
+++ Week04/Week04.3.js
@@ -0,0 +1,29 @@
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://m001-student:m001-mongodb-basics@sandbox.mrakc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(async err => {
+    if(err){
+        console.log(err.message)
+        return
+    }
+    console.log('Connected to MongoDB');
+   
+    try{
+
+        await client.connect();
+
+        const database = client.db("sample_training");
+        const collection = database.collection("trips");
+
+        const res = await collection.find({
+            "tripduration": {$lte:60}
+             }).toArray();
+        console.log(res)
+    }catch(e){
+        console.error(e);
+    }finally{
+        await client.close();
+    }
+})
